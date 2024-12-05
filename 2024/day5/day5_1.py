d8e9293df995edf899a0554f72c7a75f48c4cd2a
Pietro Trabuio <72347894+pie-trab@users.noreply.github.com>
2024-12-05 09:55:45 +0000

    day 5 part 1 and 2

--- 2024/day5/day5_1.py
+++ 2024/day5/day5_1.py
@@ -1,7 +1,7 @@
 rule_list = []
 manual_list = []
 
-with open('./sample.txt', 'r') as file:
+with open('./input.txt', 'r') as file:
     while line := file.readline():
         if not line.strip(): continue
         if '|' in line:
@@ -10,17 +10,26 @@ with open('./sample.txt', 'r') as file:
             manual_list.append(line.strip().split(','))
 
 
+valid_manuals = []
+
 for manual in manual_list:
+    is_valid = True
     for i in range(len(manual)):
         for rule in rule_list:
             ''' 
             es.
             manual: 75,47,61,53,29
-            rule: 75|53 -> (75>53)
+            rule: 75|53 -> (75 > 53)
             '''
-            if manual[i] in rule[0] and rule[1] in manual:
-                print('')
-    
-    
+            if manual[i] == rule[0] and rule[1] in manual[:i]: 
+                is_valid = False
+    if is_valid:   
+        valid_manuals.append(manual)         
 
+#print(valid_manuals)
 
+sum = 0
+for i in valid_manuals:
+    sum += int(i[int(len(i)/2)])
+
+print(sum)
