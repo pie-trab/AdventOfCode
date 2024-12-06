ee42bb8815f72ea9277f1964eab5aa3addd9b5a8
pie-trab <pietrotrabuio@gmail.com>
2024-12-06 18:51:41 +0100

    Small changes day6 part 2, still not working

--- 2024/day6/day6_2.py
+++ 2024/day6/day6_2.py
@@ -3,79 +3,80 @@
 import numpy as np
 
 with open('./sample.txt', 'r') as file_in:
-    txt = [*str(file_in.read()).replace('\n','')]
+    txt = [*str(file_in.read()).replace('\n', '')]
 
 size = 10
-matrix = np.array(txt).reshape(size,size)
+matrix = np.array(txt).reshape(size, size)
+
 
 def find_initial_pos(matrix):
     for row in range(len(matrix)):
         for col in range(len(matrix)):
-            #print(matrice[row][col])
+            # print(matrice[row][col])
             if matrix[row][col] == '^':
                 return [row, col]
-    return -1  
+    return -1
+
 
 curr_pos = find_initial_pos(matrix)
-alredy_visited = [(curr_pos[0], curr_pos[1], 0)]
-direction = 0 # 0: up, 1: rigth, 2: down, 3: left
+alredy_visited = [(curr_pos[0], curr_pos[1])]
+direction = 0  # 0: up, 1: rigth, 2: down, 3: left
 in_lab = True
 count = 0
 while in_lab:
-        match direction:    
-            case 0:
-                if curr_pos[0] - 1 < 0:
-                    in_lab = False
-                    break 
-                if matrix[curr_pos[0] - 1][curr_pos[1]] == '#':
-                    direction = (direction + 1) % 4
+    match direction:
+        case 0:
+            if curr_pos[0] - 1 < 0:
+                in_lab = False
+                break
+            if matrix[curr_pos[0] - 1][curr_pos[1]] == '#':
+                direction = (direction + 1) % 4
+            else:
+                curr_pos = [curr_pos[0] - 1, curr_pos[1]]
+                if (curr_pos[0], curr_pos[1]) in alredy_visited:
+                    count += 1
                 else:
-                    curr_pos = [curr_pos[0] - 1, curr_pos[1]]
-                    if (curr_pos[0], curr_pos[1], 0) in alredy_visited:
-                       count += 1
-                    else:
-                       alredy_visited.append((curr_pos[0], curr_pos[1], 0))
-                    matrix[curr_pos[0]][curr_pos[1]] = 'O'
-            case 1:
-                if curr_pos[1] + 1 > size - 1:
-                    in_lab = False
-                    break 
-                if matrix[curr_pos[0]][curr_pos[1] + 1] == '#':
-                    direction = (direction + 1) % 4
+                    alredy_visited.append((curr_pos[0], curr_pos[1]))
+                matrix[curr_pos[0]][curr_pos[1]] = 'O'
+        case 1:
+            if curr_pos[1] + 1 > size - 1:
+                in_lab = False
+                break
+            if matrix[curr_pos[0]][curr_pos[1] + 1] == '#':
+                direction = (direction + 1) % 4
+            else:
+                curr_pos = [curr_pos[0], curr_pos[1] + 1]
+                if (curr_pos[0], curr_pos[1]) in alredy_visited:
+                    count += 1
                 else:
-                    curr_pos = [curr_pos[0], curr_pos[1] + 1]
-                    if (curr_pos[0], curr_pos[1], 1) in alredy_visited:
-                       count += 1
-                    else:
-                       alredy_visited.append((curr_pos[0], curr_pos[1], 1))
-                    matrix[curr_pos[0]][curr_pos[1]] = 'O'
-            case 2:
-                if curr_pos[0] + 1 > size - 1:
-                    in_lab = False
-                    break 
-                if matrix[curr_pos[0] + 1][curr_pos[1]] == '#':
-                    direction = (direction + 1) % 4
+                    alredy_visited.append((curr_pos[0], curr_pos[1]))
+                matrix[curr_pos[0]][curr_pos[1]] = 'O'
+        case 2:
+            if curr_pos[0] + 1 > size - 1:
+                in_lab = False
+                break
+            if matrix[curr_pos[0] + 1][curr_pos[1]] == '#':
+                direction = (direction + 1) % 4
+            else:
+                curr_pos = [curr_pos[0] + 1, curr_pos[1]]
+                if (curr_pos[0], curr_pos[1]) in alredy_visited:
+                    count += 1
                 else:
-                    curr_pos = [curr_pos[0] + 1, curr_pos[1]]
-                    if (curr_pos[0], curr_pos[1], 2) in alredy_visited:
-                       count += 1
-                    else:
-                       alredy_visited.append((curr_pos[0], curr_pos[1], 2))
-                    matrix[curr_pos[0]][curr_pos[1]] = 'O'
-            case 3:
-                if curr_pos[1] - 1 < 0:
-                    in_lab = False
-                    break 
-                if matrix[curr_pos[0]][curr_pos[1] - 1] == '#':
-                    direction = (direction + 1) % 4
+                    alredy_visited.append((curr_pos[0], curr_pos[1]))
+                matrix[curr_pos[0]][curr_pos[1]] = 'O'
+        case 3:
+            if curr_pos[1] - 1 < 0:
+                in_lab = False
+                break
+            if matrix[curr_pos[0]][curr_pos[1] - 1] == '#':
+                direction = (direction + 1) % 4
+            else:
+                curr_pos = [curr_pos[0], curr_pos[1] - 1]
+                if (curr_pos[0], curr_pos[1]) in alredy_visited:
+                    count += 1
                 else:
-                    curr_pos = [curr_pos[0], curr_pos[1] - 1]
-                    if (curr_pos[0], curr_pos[1], 3) in alredy_visited:
-                       count += 1
-                    else:
-                       alredy_visited.append((curr_pos[0], curr_pos[1], 3))
-                    matrix[curr_pos[0]][curr_pos[1]] = 'O'
+                    alredy_visited.append((curr_pos[0], curr_pos[1]))
+                matrix[curr_pos[0]][curr_pos[1]] = 'O'
 
-        
-        print(matrix) # uncomment to show the path taken each step
+    print(matrix)  # uncomment to show the path taken each step
 print(count)
